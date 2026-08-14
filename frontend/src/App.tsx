c115e5b43906e013440935d1fb062c66ef2aeeb7
WolfY <david.skliarov.dev@gmail.com>
2026-08-14 17:02:58 +0400

    feat: add route to the main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,18 @@
-function App() {}
+import { Route, Routes } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
+import Users from "./pages/users/Users";
+
+function App() {
+	return (
+		<>
+			<MainNavigation />
+			<main className="mt-20">
+				<Routes>
+					<Route path="/" element={<Users />} />
+				</Routes>
+			</main>
+		</>
+	);
+}
 
 export default App;
