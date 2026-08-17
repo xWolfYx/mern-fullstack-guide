7dfddb5c86436b1cac679c4e60d3ff2c8c9e3df8
WolfY <david.skliarov.dev@gmail.com>
2026-08-17 16:20:46 +0400

    feat: finish styling NavLinks component

--- frontend/src/components/layout/NavLinks.tsx
+++ frontend/src/components/layout/NavLinks.tsx
@@ -1,32 +1,31 @@
 import { NavLink } from "react-router-dom";
 
+const links: { to: string; label: string }[] = [
+	{ to: "/", label: "All users" },
+	{ to: "/places", label: "My places" },
+	{ to: "/add", label: "Add place" },
+	{ to: "/auth", label: "Authenticate" },
+];
+
 export default function NavLinks() {
-	// !TODO: needs styling
-
-	const getActiveClass = ({ isActive }) => (isActive ? "bg-[#ffd900]" : "");
-
 	return (
-		<ul className="flex items-center gap-2">
-			<li>
-				<NavLink to="/" className={getActiveClass}>
-					All users
-				</NavLink>
-			</li>
-			<li>
-				<NavLink to="/places" className={getActiveClass}>
-					My places
-				</NavLink>
-			</li>
-			<li>
-				<NavLink to="/add" className={getActiveClass}>
-					Add place
-				</NavLink>
-			</li>
-			<li>
-				<NavLink to="/auth" className={getActiveClass}>
-					Authenticate
-				</NavLink>
-			</li>
+		<ul className="flex md:flex-row flex-col justify-center items-center gap-2 m-0 p-0 size-full">
+			{links.map(({ to, label }) => (
+				<li key={label} className="m-4 md:mx-2 md:my-0">
+					<NavLink
+						to={to}
+						className={({ isActive }) =>
+							`hover:bg-[#f8df00] hover:text-[#292929] p-2 border hover:border-[#292929] text-[#292929] uppercase transition duration-150 ${
+								isActive
+									? "bg-[#ffd900] border-[#292929] md:text-[#292929]"
+									: "md:text-white border-transparent"
+							}`
+						}
+					>
+						{label}
+					</NavLink>
+				</li>
+			))}
 		</ul>
 	);
 }
