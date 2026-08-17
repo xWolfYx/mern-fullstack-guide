e81ff51eafb1190ddcd18a2d00c2c5a5a9334d9b
WolfY <david.skliarov.dev@gmail.com>
2026-08-17 16:21:47 +0400

    refactor: add missing utility classes

--- frontend/src/components/layout/MainHeader.tsx
+++ frontend/src/components/layout/MainHeader.tsx
@@ -1,6 +1,6 @@
 export default function MainHeader({ children }) {
 	return (
-		<header className="top-0 left-0 fixed flex items-center md:content-between bg-[#ff0055] shadow-[0_2px_6px_rgba(0,0,0,0.26)] px-4 py-0 w-full h-16">
+		<header className="top-0 left-0 z-5 fixed flex md:justify-between items-center bg-[#ff0055] shadow-[0_2px_6px_rgba(0,0,0,0.26)] px-4 py-0 w-full h-16">
 			{children}
 		</header>
 	);
