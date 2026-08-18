e23ae4fb6d6cc4c8a200602d9f59fb81dd03584e
WolfY <david.skliarov.dev@gmail.com>
2026-08-18 16:59:50 +0400

    refactor: add styles prop

--- frontend/src/components/UI/Card.tsx
+++ frontend/src/components/UI/Card.tsx
@@ -1,6 +1,14 @@
-export default function Card({ children }) {
+export default function Card({
+	children,
+	styles,
+}: {
+	children: React.ReactNode;
+	styles?: string;
+}) {
 	return (
-		<div className="relative bg-white shadow-[0_2px_8px_rgba(0,0,0,0.26)] m-0 p-0 rounded-md overflow-hidden cursor-pointer">
+		<div
+			className={`${styles} bg-white shadow-[0_2px_8px_rgba(0,0,0,0.26)] m-0 p-4 rounded-md overflow-hidden`}
+		>
 			{children}
 		</div>
 	);
