2e6c2b790f0de0a5edf18b0e43554ae2c7d6b64e
WolfY <david.skliarov.dev@gmail.com>
2026-08-19 17:14:56 +0400

    refactor: add children prop to the Backgrop component

--- frontend/src/components/UI/Backdrop.tsx
+++ frontend/src/components/UI/Backdrop.tsx
@@ -1,7 +1,7 @@
 import { motion } from "motion/react";
 import { createPortal } from "react-dom";
 
-export default function Backdrop({ onCloseDrawer }) {
+export default function Backdrop({ onClose, children }) {
 	return createPortal(
 		<motion.button
 			initial={{ opacity: 0 }}
@@ -9,8 +9,10 @@ export default function Backdrop({ onCloseDrawer }) {
 			exit={{ opacity: 0 }}
 			type="button"
 			className="top-0 left-0 z-10 fixed bg-[rgba(0,0,0,0.75)] w-full h-screen"
-			onClick={onCloseDrawer}
-		></motion.button>,
+			onClick={onClose}
+		>
+			{children}
+		</motion.button>,
 		document.body,
 	);
 }
