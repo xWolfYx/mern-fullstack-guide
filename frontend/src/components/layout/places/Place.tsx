17bf94102c253d0949a805e4676a61323e3b5f08
WolfY <david.skliarov.dev@gmail.com>
2026-08-20 14:39:10 +0400

    feat: implement show modal on a place map button

--- frontend/src/components/layout/places/Place.tsx
+++ frontend/src/components/layout/places/Place.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+import Button from "../../UI/Button";
 import Card from "../../UI/Card";
+import Modal from "../../UI/Modal";
 
 export default function Place({
 	id,
@@ -9,23 +12,45 @@ export default function Place({
 	creatorId,
 	location,
 }) {
+	const [showMap, setShowMap] = useState(false);
+
+	const openMapHandler = () => setShowMap(true);
+	const closeMapHandler = () => setShowMap(false);
+
 	return (
-		<li className="mx-0 my-4">
-			<Card styles="p-0!">
-				<div className="mr-6 w-full h-100">
-					<img src={imageUrl} alt={title} className="size-full object-cover" />
-				</div>
-				<div className="p-4 text-center">
-					<h2 className="font-semibold text-2xl">{title}</h2>
-					<h3>{address}</h3>
-					<p>{description}</p>
-				</div>
-				<div className="flex justify-center gap-3 *:hover:bg-[#ff0055] p-4 *:py-2 *:border border-[#ccc] *:border-[#292929] *:hover:border-transparent border-t *:rounded-2xl *:w-100/3 *:hover:text-white text-center *:uppercase *:hover:scale-110 *:transition *:duration-150 *:cursor-pointer">
-					<button type="button">View on map</button>
-					<button type="button">Edit</button>
-					<button type="button">Delete</button>
-				</div>
-			</Card>
-		</li>
+		<>
+			<Modal
+				headerText={title}
+				showMap={showMap}
+				onClose={closeMapHandler}
+				contentStyles="p-0"
+				footerStyles="text-right"
+				footer={<Button onClick={closeMapHandler}>Close</Button>}
+			>
+				<div className="w-full h-60">The map!</div>
+			</Modal>
+
+			<li className="mx-0 my-4">
+				<Card styles="p-0!">
+					<div className="mr-6 w-full h-100">
+						<img
+							src={imageUrl}
+							alt={title}
+							className="size-full object-cover"
+						/>
+					</div>
+					<div className="p-4 text-center">
+						<h2 className="font-semibold text-2xl">{title}</h2>
+						<h3>{address}</h3>
+						<p>{description}</p>
+					</div>
+					<div className="flex *:flex-1 justify-around items-stretch gap-3 p-4 border-[#ccc] border-t">
+						<Button onClick={openMapHandler}>View on map</Button>
+						<Button type="router">edit</Button>
+						<Button>Delete</Button>
+					</div>
+				</Card>
+			</li>
+		</>
 	);
 }
